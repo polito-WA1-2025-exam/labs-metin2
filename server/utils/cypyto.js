2b4fabc23a16b671329981fc45944c67a16a42fe
GalileoL <galileoliu.cn@gmail.com>
2025-04-11 23:23:43 +0200

    feat: implement passport authentication with bcrypt for user login

--- server/utils/cypyto.js
+++ server/utils/cypyto.js
@@ -1,1 +1,51 @@
+"use strict";
+/*
+    A utility module for handling cryptographic operations.
+    This module provides functions to hash passwords and verify hashes.
+*/
+const crypto = require("crypto");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcrypt");
+const userDao = require("../dao/userDao");
 
+// Function to hash a password using bcrypt
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    async (username, password, done) => {
+      try {
+        const user = await userDao.getUserByUsername(username);
+        if (!user) {
+          return done(null, false, { message: "Incorrect username." });
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+// Function to serialize user
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+// Function to deserialize user
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await userDao.getUserById(id);
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+// Export the functions for use in other modules
